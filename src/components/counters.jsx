c7eb51fae4b5191b834ff875607066390749399c
Ali Ahmad <ali.ahmad@sixlogics.local>
2019-07-29 17:19:28 +0500

    add delete button

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,11 +10,21 @@ class Counters extends Component {
       { id: 4, value: 0 }
     ]
   };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value} />
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter}
+          />
         ))}
       </div>
     );
@@ -22,3 +32,11 @@ class Counters extends Component {
 }
 
 export default Counters;
+
+// import React from "react";
+
+// function Hello() {
+//   return <h1>Hello world</h1>;
+// }
+
+// export default Hello;
